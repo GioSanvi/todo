4b4e7b716c12e94e255a285fa4d47510c1c7a7d9
Giorgia <g.sanvito91@gmail.com>
2023-06-25 12:08:08 +0200

    Add todo routes

---
+++ todo_api/todo/serializers.py
@@ -0,0 +1,11 @@
+from rest_framework import serializers
+from .models import Todo
+
+
+class TodoSerializer(serializers.ModelSerializer):
+    user = serializers.ReadOnlyField(source='user.username')
+    user_id = serializers.ReadOnlyField(source='user.id')
+
+    class Meta:
+        model = Todo
+        fields = ['id', 'title', 'user', 'user_id', 'created_at', 'completed']
--- todo_api/todo/views.py
+++ todo_api/todo/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from rest_framework import generics
+from .models import Todo
+from .serializers import TodoSerializer
 
-# Create your views here.
+
+class Todos(generics.ListCreateAPIView):
+    queryset = Todo.objects.all()
+    serializer_class = TodoSerializer
